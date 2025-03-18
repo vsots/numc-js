f20f82ac8399c629ec82093ff9cc64c383124a84
vsots <vsotsdev@gmail.com>
2025-03-18 10:29:27 -0700

    completes basic cjsarray creation

--- src/index.ts
+++ src/index.ts
@@ -3,20 +3,36 @@ type CJSType = 'JS_INT' | 'JS_UINT' | 'JS_FLOAT';
 interface CJSArray {
     cjsType: CJSType;
     shape: number[];
-    array: Array<number | Array<number>>;
+    array: unknown[];
 }
 
-const makeCJSarray = <T extends unknown[]>(...params: T): CJSArray => {
-    if (!Array.isArray(params[0])) {
-        const array = params as number[];
-        let currentType: CJSType = "JS_INT";
-        let hasNegatives = false;
-        array.forEach((num) => {
-            if (num < 0) hasNegatives = true;
-            if (num > 2147483647 && !hasNegatives) currentType = "JS_UINT";
-            if (num % 1 !== 0) currentType = "JS_FLOAT";
-        })
-        return { cjsType: currentType, shape: [array.length], array } as CJSArray
+const makeCJSArray = <T extends unknown[]>(numbers: T): CJSArray => {
+    const shape = [numbers.length];
+    let elem = numbers[0];
+    if (Array.isArray(elem)) {
+        while (Array.isArray(elem)) {
+            shape.push(elem.length);
+            elem = elem[0];
+        }
     }
-    return { cjsType: "JS_INT", shape: [1], array: [1, 2, 3, 4] };
+
+    let currentType: CJSType = "JS_INT";
+    let hasNegatives = false;
+    const discernType = (num: number): CJSType => {
+        if (num < 0) hasNegatives = true;
+        else if (num > 2147483647 && !hasNegatives) currentType = "JS_UINT";
+        if (num % 1 !== 0) currentType = "JS_FLOAT";
+        return currentType;
+    }
+
+    const loopNArray = (arr: unknown[]) => {
+        for (let i = 0; i < arr.length; i++) {
+            if (Array.isArray(arr[i])) loopNArray(arr[i] as unknown[]);
+            else currentType = discernType(arr[i] as number);
+        }
+    }
+
+    loopNArray(numbers);
+
+    return { cjsType: currentType, shape, array: numbers } as CJSArray
 };
